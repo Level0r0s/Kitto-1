c5a320f9732cbc2dfb21a49a1a0e6cf8c6677768
kirankoirala <kinbech@gmail.com>
2015-02-26 20:43:13 -0500

    added a failing test to continue next day

--- KO.Unit.Tests/Controller/CustomerControllerTests.cs
+++ KO.Unit.Tests/Controller/CustomerControllerTests.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KP.Web.Controllers;
+using System.Web.Mvc;
 
 namespace KO.Unit.Tests.Controller
 {
-    class CustomerControllerTests
+    [TestFixture]
+    public class CustomerControllerTests
     {
+        [Test]
+        public void Index_Returns_A_View()
+        {
+            var controller = new CustomerController();
+            var result = controller.Index();
+
+            Assert.IsInstanceOf<ActionResult>(result);
+        }
+
+        [Test]
+        public void Index_Calls_CustomerRequest_To_Get_AvailableCustomers()
+        {
+            Assert.Fail();
+        }
     }
 }
